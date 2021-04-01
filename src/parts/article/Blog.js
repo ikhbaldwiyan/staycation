d578c39d274d4aa247aa463d53483e592bf30e77
inzoid <inzoid21@gmail.com>
2021-04-01 13:38:19 +0700

    styling article tag and add icon date

--- src/parts/article/Blog.js
+++ src/parts/article/Blog.js
@@ -2,6 +2,7 @@ import React from "react";
 import Fade from "react-reveal/Fade";
 import ReactHtmlParser from "react-html-parser";
 import Button from "elements/Button";
+import IconDate from "assets/images/icons/icon_cities.svg";
 
 export default function Blog({ data }) {
   return data.map((article, idx) => (
@@ -10,17 +11,16 @@ export default function Blog({ data }) {
         <Fade bottom delay={300 * idx}>
           <div className="item column-4 mr-3">
             <div className="card h-100">
-              <div className="tag" style={{borderRadius: '0 10px'}}>
-                {article.category}
-              </div>
+              <div className="tag-article">{article.category}</div>
               <figure className="img-article">
                 <img className="img-cover" src={article.url} alt={data.id} />
               </figure>
             </div>
           </div>
-          <div className="item column-7">
+          <div className="item column-6">
             <h3>{article.title}</h3>
             <span className="text-gray-500">
+              <img src={IconDate} width={18} className="ml-2 mb-1" alt="date" />{" "}
               {article.author}, {article.published}
             </span>
             <p className="mt-3 text-gray-700">
