1efc3b1aaa5df7a1c8ae97cc7ad45ad333cbf468
inzoid <inzoid21@gmail.com>
2021-06-23 13:29:27 +0700

    update home article and search function

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -6,10 +6,11 @@ import Blog from "parts/article/Blog";
 
 function Article(props) {
   const [article, setArticle] = useState([]);
+  const [search, setSearch] = useState('');
   const baseUrl = "https://the-lazy-media-api.vercel.app/api/";
-  const articleTags = 'console-game';
+  const articleTags = 'pc';
   const page = '1';
-  const api = `${baseUrl}games/${articleTags}?page=${page}`
+  const api = `${baseUrl}search?search=${search}`
 
   useEffect(() => {
     window.document.title = "Article";
@@ -17,13 +18,16 @@ function Article(props) {
       const data = res.data;
       setArticle(data)
     });
-  }, [article]);
+  }, [api]);
 
   return (
     <>
       <Header {...props} />
       <div className="container">
         <div className="container mb-4">
+          <input className="mb-2" type="text" placeholder=" Search Article" onChange={(e) => {
+            setSearch(e.target.value)
+          }} />
           <h2>Latest Article</h2>
         </div>
         <Blog data={article} />
--- src/pages/DetailBlog.js
+++ src/pages/DetailBlog.js
@@ -5,64 +5,103 @@ import ReactHtmlParser from "react-html-parser";
 
 import Header from 'parts/Header';
 import Breadcrumb from 'elements/Breadcrumb';
+import Footer from 'parts/Footer';
 
 function DetailBlog(props) {
   const [detail, setDetail] = useState({});
   const slug = window.location.pathname;
 
   useEffect(() => {
-    window.document.title = 'Detail Artikel';
+    window.document.title = detail.title;
     axios
       .get(`https://the-lazy-media-api.vercel.app/api${slug}`)
       .then((res) => {
         const data = res.data.results;
         setDetail(data);
       });
-    console.log(detail);
-  }, []);
+  }, [detail, slug]);
+
+  let description = [];
+
+  if(detail.content) {
+    for (let i = 0; i < detail.content.length; i++) {
+      let desc = detail.content[i];
+      desc += ' <br> <br> '
+  
+      if (desc.includes('https://thelazy.media')) {
+        let image = desc += formatDescription(desc)
+        description.push(image)
+      }
+      
+      if (desc.includes(',')) {
+        description.push(desc)
+      }
+    }
+  }
+
+  function formatDescription(text) {
+    return (text || "").replace(
+      /([^\S]|^)(((https?\:\/\/)|(www\.))(\S+))/gi,
+      function (match, space, url) {
+        let hyperlink = url;
+        if (!hyperlink.match("^https?://")) {
+          hyperlink = "http://" + hyperlink;
+        }
+        let image =  hyperlink
+        return space + '<img class="img-cover" src="' + image + '" />';
+      }
+    );
+  }
 
   const breadcrumb = [
     { pageTitle: "Article", pageHref: "/article" },
     { pageTitle: detail.categories && detail.categories[0] , pageHref: "" },
+    { pageTitle: detail.categories && detail.categories[1] , pageHref: "" },
   ];
 
-  const description = detail.content;
 
   return (
     <div>
       <Header {...props} />
-      <section className="container spacing-sm">
-        <Fade bottom>
-          <div className="row align-items">
-            <div className="col">
-              <Breadcrumb data={breadcrumb} />
+      <div className="container">
+        <section className="container spacing-sm">
+          <Fade bottom>
+            <div className="row align-items">
+              <div className="col-12">
+                <Breadcrumb data={breadcrumb} className="mr-3" />
+              </div>
+              <div className="col-8 text-left">
+                <h1 className="h4">{detail.title}</h1>
+                <span className="text-gray-500">
+                  {detail.author} - {detail.date}
+                </span>
+              </div>
+              <div className="col"></div>
             </div>
-            <div className="col-auto text-center">
-              <h1 className="h4">{detail.title}</h1>
-              <span className="text-gray-500">
-                {detail.author}, {detail.date}
-              </span>
+          </Fade>
+          <div className="container-grid sm mt-3">
+            <div className="item column-8">
+              <div className="card h-100">
+                <figure className="img-wrapper">
+                  <img
+                    className="img-cover"
+                    src={detail.thumb ?? 'https://i.stack.imgur.com/y9DpT.jpg'}
+                    alt={detail.title}
+                  />
+                </figure>
+              </div>
             </div>
-            <div className="col"></div>
           </div>
-        </Fade>
-        <div className="container-grid sm mt-3">
-          <div className="item column-12">
-            <div className="card h-100">
-              <figure className="img-wrapper">
-                <img
-                  className="img-cover"
-                  src={detail.thumb ?? 'https://i.stack.imgur.com/y9DpT.jpg'}
-                  alt={detail.title}
-                />
-              </figure>
+          <div className="mt-3">
+            <div className="row">
+              <div className="col-8">
+              <p>{ReactHtmlParser(description)}</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mt-3">
-          <p>{description}</p>
-        </div>
-      </section>
+        </section>
+      </div>
+      <Footer />
     </div>
   );
 }
